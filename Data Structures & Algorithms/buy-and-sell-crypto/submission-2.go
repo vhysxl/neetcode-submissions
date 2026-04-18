b3d0715e7b0cff9a9f630bc25a521cefe2dee878
vhysxl <126753148+vhysxl@users.noreply.github.com>
2026-04-18 10:21:25 +0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.go
@@ -0,0 +1,14 @@
+func maxProfit(prices []int) int {
+	minPrice := prices[0]
+	maxProfit := 0
+
+	for _, price := range prices {
+		if price < minPrice {
+			minPrice = price
+		} else if price-minPrice > maxProfit {
+			maxProfit = price - minPrice
+		}
+	}
+
+	return maxProfit
+}
