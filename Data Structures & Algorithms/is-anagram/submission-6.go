e76b6713271eccd544fe3d83571e56820d296f37
vhysxl <126753148+vhysxl@users.noreply.github.com>
2026-05-25 21:17:36 +0700

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.go
@@ -0,0 +1,20 @@
+func isAnagram(s string, t string) bool {
+
+	charCount := make(map[string]int)
+
+	for _, v := range s {
+		charCount[string(v)] += 1
+	}
+
+	for _, v := range t {
+		charCount[string(v)] -= 1
+	}
+
+	for _, v := range charCount {
+		if v != 0 {
+			return false
+		}
+	}
+
+	return true
+}
