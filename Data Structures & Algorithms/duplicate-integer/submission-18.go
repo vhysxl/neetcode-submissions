0318f3fdb71163edc83d390c3c01d87e90654f7e
vhysxl <126753148+vhysxl@users.noreply.github.com>
2026-06-13 23:59:51 +0700

    Add: duplicate-integer - submission-18

---
+++ Data Structures & Algorithms/duplicate-integer/submission-18.go
@@ -0,0 +1,13 @@
+func hasDuplicate(nums []int) bool {
+	seen := make(map[int]bool)
+
+	for _, num := range nums {
+		if seen[num] {
+			return true
+		}
+
+		seen[num] = true
+	}
+
+	return false
+}
